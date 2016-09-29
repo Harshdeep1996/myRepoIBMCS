994ca362381c1675863b2e2ffd7bcf9c3f800cb3
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-09-29 16:25:25 +0100

    # This is a combination of 2 commits. # The first commit's message is:
    # This is a combination of 7 commits.
    # The first commit's message is:
    updating travis
    
    # This is the 2nd commit message:
    
    fdnjfonfsnff
    
    # This is the 3rd commit message:
    
    testing
    
    # This is the 4th commit message:
    
    testing
    
    # This is the 5th commit message:
    
    testing
    
    # This is the 6th commit message:
    
    getting screenshots
    
    # This is the 7th commit message:
    
    hsuddhsu
    
    # This is the 2nd commit message:
    
    bddd

--- get_screenshot.py
+++ get_screenshot.py
@@ -1,11 +1,15 @@
 import validators
 from selenium import webdriver
 from pyvirtualdisplay import Display
+from fogbugz import FogBugz
+
+fb = FogBugz("https://cloudant.fogbugz.com")
+fb.logon("harshdeep.harshdeep@uk.ibm.com","Harsh1996")
 
 def main(source,times=None):
 	display = Display(visible=0, size=(800, 600))
 	display.start()
-	driver = webdriver.Chrome() #Using chrome web driver
+	driver = webdriver.Firefox() #Using chrome web driver
 	if not validators.url(source): 
 		driver.get('file://' + source)
 	else:
@@ -18,13 +22,17 @@ def how_many_screenshots(driver,times):
 	elementHeight = driver.find_element_by_tag_name("body").size['height'] 
 	if times is None:
 		driver.save_screenshot('screenshot_full.png')
+		file = open('screenshot_full.png', 'r')
+		fb.edit(ixBug=73752, Files={'screenshot_full.png': file})
 	else:
 		portions = 0
 		temp = elementHeight / times
 		while(portions < elementHeight):
 			driver.execute_script("window.scrollBy(0, %d);" % portions)
 			driver.save_screenshot('screenshot_full_%d.png' % portions)
+			file = open('screenshot_full_%d.png' % portions, 'r')
+			fb.edit(ixBug=73752, Files={'screenshot_full_%d.png' % portions: file})
 			portions += temp
 
 if __name__ == '__main__':
-    main('./output.html',times=4)
+    main('https://www.google.com',times=None)
