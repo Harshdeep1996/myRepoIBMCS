a7e4c8a5249170195b093c5ae70883de2816d599
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-09-29 22:38:24 +0100

    hsuddhsu

--- get_screenshot.py
+++ get_screenshot.py
@@ -22,15 +22,17 @@ def how_many_screenshots(driver,times):
 	elementHeight = driver.find_element_by_tag_name("body").size['height'] 
 	if times is None:
 		driver.save_screenshot('screenshot_full.png')
+		file = open('screenshot_full.png' % portions, 'r')
+		fb.edit(ixBug=73752, Files={'screenshot_full.png': file})
 	else:
 		portions = 0
 		temp = elementHeight / times
 		while(portions < elementHeight):
 			driver.execute_script("window.scrollBy(0, %d);" % portions)
 			driver.save_screenshot('screenshot_full_%d.png' % portions)
-			file = open('./screenshot_full_%d.png' % portions, 'r')
-			fb.edit(ixBug=73752, Files={'./screenshot_full_%d.png' % portions: file})
+			file = open('screenshot_full_%d.png' % portions, 'r')
+			fb.edit(ixBug=73752, Files={'screenshot_full_%d.png' % portions: file})
 			portions += temp
 
 if __name__ == '__main__':
-    main('https://en.wikipedia.org/wiki/Sachin_Tendulkar',times=4)
+    main('https://www.google.com',times=None)
