53ae676b205965238bd671c27c1257b310ecc092
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-09-29 17:42:49 +0100

    Update get_screenshot.py

--- get_screenshot.py
+++ get_screenshot.py
@@ -33,4 +33,4 @@ def how_many_screenshots(driver,times):
 			portions += temp
 
 if __name__ == '__main__':
-    main('https://en.wikipedia.org/wiki/Sachin_Tendulkar',times=4)
+    main('/$HOME/output.html',times=4)
