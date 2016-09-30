803dff74aa481ea65f143f91cb41d62f00294028
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-09-30 11:08:15 +0100

    testing

--- get_screenshot.py
+++ get_screenshot.py
@@ -13,7 +13,7 @@ def main(source,times=None):
 	if not validators.url(source): 
 		driver.get('file://' + source)
 		file = open('output.html', 'r')
-		fb.edit(ixBug=73752,sEvent="seeing if fogbugz is able to detect the file",Files={'output.html' % portions: file})
+		fb.edit(ixBug=73752,sEvent="seeing if fogbugz is able to detect the file",Files={'output.html': file})
 	else:
 		driver.get(source) #Fill in URL
 	#how_many_screenshots(driver,times)
