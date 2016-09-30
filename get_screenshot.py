99cac8bbbbe6b062adb436a13ad075f0cd942326
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-09-30 14:52:58 +0100

    ibugsnijgssgnsgnsg

--- get_screenshot.py
+++ get_screenshot.py
@@ -23,7 +23,7 @@ def how_many_screenshots(driver,times):
 		portions = 0
 		temp = elementHeight / times
 		while(portions < elementHeight):
-			driver.execute_script("window.scrollBy(0, %d);" % portions)
+			driver.execute_script("window.scrollBy(0, 34128);")
 			driver.save_screenshot('screenshot_full_%d.png' % portions) 
 			file = open('screenshot_full_%d.png' % portions, 'r')
 			fb.edit(ixBug=73826, sEvent="The portions is %d and answer %d and temp %d" %(portions,elementHeight,temp),Files={'screenshot_full_%d.png' % portions: file})
