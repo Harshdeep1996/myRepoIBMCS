e59416675e71e35f61d8081811ca00466579276b
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-09-30 14:47:27 +0100

    ibugsnijgssgnsgnsg

--- get_screenshot.py
+++ get_screenshot.py
@@ -7,10 +7,10 @@ fb = FogBugz("https://cloudant.fogbugz.com")
 fb.logon("harshdeep.harshdeep@uk.ibm.com","Harsh1996")
 
 def main(source,times=None):
-	display = Display(visible=0, size=(800, 600))
+	display = Display(visible=0, size=(1024, 768))
 	display.start()
-	driver = webdriver.Firefox()
-	driver.get(source)
+	driver = webdriver.Firefox() #Using chrome web driver
+	driver.get(source) #Fill in URL
 	how_many_screenshots(driver,times)
 	driver.quit()
 	display.stop()
@@ -19,18 +19,19 @@ def how_many_screenshots(driver,times):
 	elementHeight = driver.find_element_by_tag_name("body").size['height'] 
 	if times is None:
 		driver.save_screenshot('screenshot_full.png')
-		file = open('screenshot_full.png', 'r')
-		fb.edit(ixBug=73752, Files={'screenshot_full.png': file})
 	else:
 		portions = 0
 		temp = elementHeight / times
 		while(portions < elementHeight):
-			x = driver.execute_script("window.scrollBy(0, %d); return 5;" % portions)
+			driver.execute_script("window.scrollBy(0, %d);" % portions)
 			driver.save_screenshot('screenshot_full_%d.png' % portions) 
 			file = open('screenshot_full_%d.png' % portions, 'r')
-			fb.edit(ixBug=73752, sEvent="The portions is %d and answer %d and height %d and temp %d" %(portions,x,elementHeight,temp),Files={'screenshot_full_%d.png' % portions: file})
+			fb.edit(ixBug=73826, sEvent="The portions is %d and answer %d and temp %d" %(portions,elementHeight,temp),Files={'screenshot_full_%d.png' % portions: file})
 			portions += temp
 
 if __name__ == '__main__':
 	source = "http://localhost:8000/templates/output.html"
 	main(source,times=4)
+
+
+
