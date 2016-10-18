483ee2b9cf9e225b97e4d5f176d3fa6171f542c4
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-18 16:07:11 +0100

    fnjfdjnfdiuhfdiunfdi

--- get_token_travis.py
+++ get_token_travis.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 from datetime import timedelta
 
 
-git.Git().clone("git@github.com:Harshdeep1996/test-data.git")
-g = git.Git('test-data')
+git.Git().clone("git@github.com:Harshdeep1996/myRepoIBMCS.git")
+g = git.Git('myRepoIBMCS')
 file_age = dt.strptime(g.log('-1', '--pretty="%cI"', 'test.xml')[1:-7], "%Y-%m-%dT%H:%M:%S") + timedelta(minutes=60)
 sys.stderr.write("Total time lapsed : %s \n" %str((dt.now() - file_age).total_seconds()))
