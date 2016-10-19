d0d486b45bed58b826d717bbe0bddfef6303f58a
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-19 09:45:57 +0100

    testing for file timing

--- get_token_travis.py
+++ get_token_travis.py
@@ -4,8 +4,6 @@ import git
 from datetime import datetime as dt
 from datetime import timedelta
 
-
-git.Git().clone("git@github.com:Harshdeep1996/myRepoIBMCS.git")
-g = git.Git('myRepoIBMCS')
+g = git.Git()
 file_age = dt.strptime(g.log('-1', '--pretty="%cI"', 'test.xml')[1:-7], "%Y-%m-%dT%H:%M:%S") + timedelta(minutes=60)
 sys.stderr.write("Total time lapsed : %s \n" %str((dt.now() - file_age).total_seconds()))
