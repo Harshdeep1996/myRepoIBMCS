0422d2e966868d828cc6ac7a6d219d71f56bd887
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-19 12:26:58 +0100

    dndfkdfn

--- get_token_travis.py
+++ get_token_travis.py
@@ -15,11 +15,10 @@ print g.log("-1","test.xml")
 print "-------------------------------------------------------------------------------------------"
 print g.log("-1","--pretty='%ci'","test.xml")
 print "-------------------------------------------------------------------------------------------"
-time_updated = bst.localize(dt.strptime(g.log('-1', '--pretty="%ci"', 'test.xml')[1:-7], "%Y-%m-%d %H:%M:%S")).astimezone(pytz.utc)
-x = time_updated.strftime("%Y-%m-%d %H:%M:%S")
+time_updated = dt.replace(bst.localize(dt.strptime(g.log('-1', '--pretty="%ci"', 'test.xml')[1:-7], "%Y-%m-%d %H:%M:%S")).astimezone(pytz.utc), tzinfo=None)
 #print(dt.fromtimestamp(int(x)).strftime('%Y-%m-%d %H:%M:%S'))
 sys.stderr.write("The time now is %s \n" %str(time_now))
-sys.stderr.write("Age of file is: %s" %str(x))
+sys.stderr.write("Age of file is: %s" %str(time_updated))
 
-sys.stderr.write("The time difference between seconds: %s" %str((time_now - x).total_seconds()))
+sys.stderr.write("The time difference between seconds: %s" %str((time_now - time_updated).total_seconds()))
 
