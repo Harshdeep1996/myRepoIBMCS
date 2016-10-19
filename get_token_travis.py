bc69785a3117a369db746f83f8b079305e35c8b4
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-19 10:42:09 +0100

    dfjfdfdidf

--- get_token_travis.py
+++ get_token_travis.py
@@ -6,8 +6,11 @@ from datetime import timedelta
 
 g = git.Git()
 time_now = dt.now()
-print g.log()
+#print g.log()
 print g.log("-1")
+print "-------------------------------------------------------------------------------------------"
+print g.log("-1","test.xml")
+print "-------------------------------------------------------------------------------------------"
 file_age = g.log('-1', '--pretty="%cI"', 'test.xml')[1:-7]
 sys.stderr.write("The time now is %s \n" %str(time_now))
 sys.stderr.write("Age of file is: %s" %str(file_age))
