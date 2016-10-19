98362921d5f652a2304533907b1a42a2d42b81d2
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-19 11:10:45 +0100

    smfsklnmfsnfsnosfn

--- get_token_travis.py
+++ get_token_travis.py
@@ -11,8 +11,10 @@ print g.log("-1")
 print "-------------------------------------------------------------------------------------------"
 print g.log("-1","test.xml")
 print "-------------------------------------------------------------------------------------------"
-print g.log("-1","--pretty='%ct'","test.xml")
+print g.log("-1","--pretty='%ci'","test.xml")
 print "-------------------------------------------------------------------------------------------"
-file_age = g.log('-1', '--pretty="%cI"', 'test.xml')[1:-7]
+x = g.log("-1","--pretty='%ct'","test.xml")
+#print(dt.fromtimestamp(int(x)).strftime('%Y-%m-%d %H:%M:%S'))
 sys.stderr.write("The time now is %s \n" %str(time_now))
 sys.stderr.write("Age of file is: %s" %str(file_age))
+
