5674781465bbba10537fd24c25cab117c11aca08
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-19 10:24:16 +0100

    testing for file timing

--- get_token_travis.py
+++ get_token_travis.py
@@ -4,7 +4,7 @@ import git
 from datetime import datetime as dt
 from datetime import timedelta
 
-g = git.Git()
+g = git.Git('myRepoIBMCS')
 time_now = dt.now()
 file_age = g.log('-1', '--pretty="%cI"', 'test.xml')[1:-7]
 sys.stderr.write("The time now is %s \n" %str(time_now))
