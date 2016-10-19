40255e1eb50140f9beb236c06a9318be3982d663
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-19 11:35:31 +0100

    testing time

--- get_token_travis.py
+++ get_token_travis.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 from datetime import timedelta
 
 g = git.Git()
-time_now = dt.now()
+time_now = dt.utcnow()
 #print g.log()
 print g.log("-1")
 print "-------------------------------------------------------------------------------------------"
@@ -17,4 +17,6 @@ x = dt.strptime(g.log('-1', '--pretty="%ci"', 'test.xml')[1:-7], "%Y-%m-%d %H:%M
 #print(dt.fromtimestamp(int(x)).strftime('%Y-%m-%d %H:%M:%S'))
 sys.stderr.write("The time now is %s \n" %str(time_now))
 sys.stderr.write("Age of file is: %s" %str(x))
+
+sys.stderr.write("The time difference between seconds: %s" %str((time_now - x).total_seconds()))
 
