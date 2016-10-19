7616e3d21c32299dae463454b038e748870f0ba4
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-10-19 13:26:30 +0100

    dndfkdfn

--- get_token_travis.py
+++ get_token_travis.py
@@ -18,10 +18,12 @@ print "-------------------------------------------------------------------------
 time_updated = dt.replace(bst.localize(dt.strptime(g.log('-1', '--pretty="%ci"', 'test.xml')[1:-7], "%Y-%m-%d %H:%M:%S")).astimezone(pytz.utc), tzinfo=None)
 #print(dt.fromtimestamp(int(x)).strftime('%Y-%m-%d %H:%M:%S'))
 sys.stderr.write("The time now is %s \n" %str(time_now))
-sys.stderr.write("Age of file is: %s" %str(time_updated))
+sys.stderr.write("Age of file is: %s \n" %str(time_updated))
 
-sys.stderr.write("The time difference between seconds: %s" %str((time_now - time_updated).total_seconds()))
+sys.stderr.write("The time difference between seconds: %s \n" %str((time_now - time_updated).total_seconds()))
 
-if (time_now - time_updated).total_seconds() > 90000:
+sys.stderr.write("Just outside the if condition")
+if (time_now - time_updated).total_seconds() < 90000:
 	sys.stderr.write("The if condition might be working")
+sys.stderr.write("Condition is finished")
 
